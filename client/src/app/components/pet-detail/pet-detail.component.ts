6095f4c2395e0e08ceee795d8af55194011aa74d
Luis Aviles <luis.avilesz@gmail.com>
2025-10-30 17:57:09 -0400

    Add language selection for pet description in PetDetail component

--- client/src/app/components/pet-detail/pet-detail.component.ts
+++ client/src/app/components/pet-detail/pet-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, inject } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
 import { PetService } from '../../services/pet.service';
@@ -7,13 +8,14 @@ import { Pet } from '../../models/pet.model';
 
 @Component({
   selector: 'app-pet-detail',
-  imports: [CommonModule, RouterLink, NgOptimizedImage],
+  imports: [CommonModule, RouterLink, NgOptimizedImage, FormsModule],
   templateUrl: './pet-detail.component.html',
   styleUrls: ['./pet-detail.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PetDetailComponent implements OnInit {
   pet$!: Observable<Pet | undefined>;
+  selectedLanguage: 'en' | 'es' | 'fr' = 'en';
 
   private route = inject(ActivatedRoute);
   private router = inject(Router);
